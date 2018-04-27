6953e29d3fc670b8247db6bd81f498b688b1a7ca
yaamaguchi <m-sy@live.com>
2018-04-26 22:53:10 -0300

    aula 4

---
+++ src/components/shared/painel/Exemplo.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <slot name="cabecalho" class="header" ></slot>
+        <hr>
+        <slot class="body"></slot>
+        <hr>
+        <slot name="rodape" class="footer"></slot>
+    </div>
+
+
+    <!-- <componente-qualquer>
+        <div slot="cabecalho">
+            <h1>Bem-vindo!</h1>
+        </div>
+        <p>Seja bem-vindo à Alura!</p>
+        <div slot="rodape">
+            <p>copyright 2017</p>
+        </div>
+    </componente-qualquer> -->
+
+</template>
+
+<script>
+export default {
+}
+
+</script>
+
+<style scoped>
+
+</style>
